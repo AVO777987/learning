30d8694936b8d2a78a3460a9ba684a9b91465820
AVO777 <seo@ox2.ru>
2020-12-16 19:35:37 +0300

    Домашние задание к первому уроку

--- 4_func_exponentiation.py
+++ 4_func_exponentiation.py
@@ -1,17 +1,23 @@
-#def exponentiation(x, y):
-    # a = 1 / (x**y)
-    # for n in abs(y):
-    #     result = result * (1 / x)
-    # return result
+def exponentiation(*args):
+    a = (args[0][0]**args[0][1])
+    b = 1
+    for n in range(abs(args[0][1])):
+        b = b * (1 / args[0][0])
+    return a, b
 
-# exponentiation(1,-2)
+
+while True:
+    try:
+        numbers = list(map(int, input("Введите два числа через пробел которые надо возвести в степень ").split()))
+        if len(numbers) != 2:
+            print("Вы не ввели два числа")
+            continue
+        numbers[1] = abs(numbers[1]) * -1
+        print(exponentiation(numbers))
+        break
+    except:
+        print("Вы ввели не числа")
+        continue
 
 
-x = 2
-y = -3
-a = (x**y)
-b = 1
-for n in range(abs(y)):
-    b = b * (1 / x)
-print(a)
-print(b)
+
