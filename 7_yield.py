4a1015333dddcfd0921c87db2e476d4644338b0f
AVO777 <seo@ox2.ru>
2020-12-19 17:37:51 +0300

    Домашние задание к первому уроку

--- 7_yield.py
+++ 7_yield.py
@@ -1,10 +1,10 @@
 def fact(n):
     factorial = 1
-    for el in range(1, n):
+    for el in range(1, n + 1):
         factorial *= el
         yield factorial
 
-n = 10
+n = 4
 
 for el in fact(n):
     print(el)
