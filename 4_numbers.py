ced850a6cc694d94d21a2bf8ba8d184e341f8dec
AVO777987 <64902703+AVO777987@users.noreply.github.com>
2020-12-23 15:41:55 +0300

    Add files via upload

---
+++ 4_numbers.py
@@ -0,0 +1,19 @@
+try:
+    with open('my_file.txt', 'r', encoding='UTF-8-sig') as file:
+        new_list = []
+        for line in file:
+            line_number = line.split(' — ')
+            if line_number[0] == 'One':
+                line_number[0] = 'Один'
+            if line_number[0] == 'Two':
+                line_number[0] = 'Два'
+            if line_number[0] == 'Three':
+                line_number[0] = 'Три'
+            if line_number[0] == 'Four':
+                line_number[0] = 'Четыре'
+            new_list.append(line_number)
+except:
+    print("Ошибка чтения файла")
+with open('new_file.txt', 'w', encoding='UTF-8-sig') as file:
+    for line in new_list:
+        print(f'{line[0]} — {line[1]}', file=file)
